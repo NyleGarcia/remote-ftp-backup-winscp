ac06c2b8b2cf5b6f7fdae535fcdf73c8c39afc91
Bebabo10 <Nylegarcia01@gmail.com>
2017-07-19 20:20:30 -0500

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,4 +1,4 @@
-start /w "" "C:\Program Files (x86)\WinSCP\WinSCP.exe" /console /script=E:\7daystodieserver\7daystodieserverbackup\backup.txt /log=E:\7daystodieserver\7daystodieserverbackup\log.txt
+start /w "" "C:\Program Files (x86)\WinSCP\WinSCP.exe" /console /script=E:\\backup.txt /log=E:\log.txt
  
 set date=%date:~10,4%%date:~7,2%%date:~4,2%
 
